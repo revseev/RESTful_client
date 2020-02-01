f3f85282c4a30f6a9cd4f3bb1f5aa6e6b81b3b01
revseev <useless47@gmail.com>
2020-02-01 17:09:25 +0300

    + CustomUserInfoTokenService changed loadByUsername()

--- src/main/java/com/javamentor/revseev/rest/security/CustomUserInfoTokenService.java
+++ src/main/java/com/javamentor/revseev/rest/security/CustomUserInfoTokenService.java
@@ -1,5 +1,7 @@
 package com.javamentor.revseev.rest.security;
 
+import com.javamentor.revseev.rest.model.Role;
+import com.javamentor.revseev.rest.model.User;
 import com.javamentor.revseev.rest.service.UserService;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -29,6 +31,57 @@ public class CustomUserInfoTokenService extends UserInfoTokenServices {
         super(userInfoEndpointUrl, clientId);
         this.userInfoEndpointUrl = userInfoEndpointUrl;
         this.clientId = clientId;
+    }
+
+    @Override
+    public OAuth2Authentication loadAuthentication(String accessToken) throws AuthenticationException, InvalidTokenException {
+        // достаем map из токена
+        Map<String, Object> map = getMap(this.userInfoEndpointUrl, accessToken);
+        // логика поиска существующего юзера в БД и запись туда юзера на основе логина из гугла
+        String principalName = map.get("email").toString();
+        User user = userService.findByUsername(principalName);
+
+        if (user == null) {
+            String principalPassword = passwordEncoder.encode(map.get("at_hash").toString());
+            long principalMoney = Long.parseLong(map.get("sub").toString());
+
+            user = new User(principalName, principalPassword, principalMoney);
+            user.setRoles(Collections.singleton(new Role("USER")));
+
+            userService.saveUser(user);
+        }
+        // формирование
+        Authentication authentication = new UsernamePasswordAuthenticationToken(user, "", user.getAuthorities());
+        OAuth2Request request = new OAuth2Request(null, accessToken, null, true, null,
+                null, null, null, null);
+        return new OAuth2Authentication(request, authentication);
+    }
+
+    // default impl to bypass private access of getMap()
+    private Map<String, Object> getMap(String path, String accessToken) {
+        if (this.logger.isDebugEnabled()) {
+            this.logger.debug("Getting user info from: " + path);
+        }
+        try {
+            OAuth2RestOperations restTemplate = this.restTemplate;
+            if (restTemplate == null) {
+                BaseOAuth2ProtectedResourceDetails resource = new BaseOAuth2ProtectedResourceDetails();
+                resource.setClientId(this.clientId);
+                restTemplate = new OAuth2RestTemplate(resource);
+            }
+            OAuth2AccessToken existingToken = restTemplate.getOAuth2ClientContext().getAccessToken();
+            if (existingToken == null || !accessToken.equals(existingToken.getValue())) {
+                DefaultOAuth2AccessToken token = new DefaultOAuth2AccessToken(accessToken);
+                String tokenType = DefaultOAuth2AccessToken.BEARER_TYPE;
+                token.setTokenType(tokenType);
+                restTemplate.getOAuth2ClientContext().setAccessToken(token);
+            }
+            return restTemplate.getForEntity(path, Map.class).getBody();
+        }
+        catch (Exception ex) {
+            this.logger.warn("Could not fetch user details: " + ex.getClass() + ", " + ex.getMessage());
+            return Collections.<String, Object>singletonMap("error", "Could not fetch user details");
+        }
     }
 }
 
